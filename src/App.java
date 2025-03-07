4b53eb2aa9c12d8e447ced1dcbe166648d9fb1f6
juancalocutor <juancalocutor@gmail.com>
2025-03-07 17:45:43 -0500

    Ejercicio con interfaces con logo

--- src/App.java
+++ src/App.java
@@ -40,5 +40,16 @@ public class App {
             System.out.println();
         }
 
+        for (int i = 0; i < 5; i++) {
+            System.out.print("   ".repeat(5 - i));
+            System.out.println("*".repeat(2 * i + 1));
+        }
+        System.out.println("GuayuribaDev =====>|| ");
+        for (int i = 5; i > 0; i--) {
+            System.out.print("   ".repeat(5 - i + 1));
+            System.out.println("*".repeat(2 * i - 1));
+        }
+        System.out.println();
+
     }
 }
